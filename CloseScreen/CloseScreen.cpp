7dce82767b5f691f8a9ed99bb1822ab5a262a3af
MCSQNXA <3337140142@qq.com>
2021-12-11 14:54:17 +0800

    2021-12-11-14-54

--- CloseScreen/CloseScreen.cpp
+++ CloseScreen/CloseScreen.cpp
@@ -1,10 +1,5 @@
 ﻿#include <windows.h>
 
-#pragma comment(linker,"/align:128")//编译对齐
-#pragma comment(linker,"/ENTRY:start")//exe主入口
-#pragma comment(linker,"/subsystem:windows")//子系统
-
-
 void start()
 {
 	PostMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
